79ab463a1fc9c32057277b9c95e751522ae5c1d5
ManiDeep1822 <indlachinnu123@gmail.com>
2025-10-17 13:09:47 +0530

    Update React calendar app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/2400030131_skillsem_exam/',
   plugins: [react()],
-})
+});
